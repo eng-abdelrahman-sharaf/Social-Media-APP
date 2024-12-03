a74143a93b6d073165d88da55210a212c3b04a08
Anwar <abd.ahm.anwar@gmail.com>
2024-12-03 22:30:19 +0200

    finallllllllly fix problem and succ make loadlist change from hashmap to uesr 's objs

--- SocialMediaApp/src/main/java/com/programming2/socialmediaapp/UserAccountManagement/JsonStorageHandler.java
+++ SocialMediaApp/src/main/java/com/programming2/socialmediaapp/UserAccountManagement/JsonStorageHandler.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.programming2.socialmediaapp.UserAccountManagement;
 
-/**
- *
- * @author abdah
- */
-public class JsonStorageHandler {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonStorageHandler<T> implements IndividualStorageHandler<T>, ListStorageHandler<T> {
+
     
+    private final Class<T> type;
+
+    public JsonStorageHandler(Class<T> type) {
+        
+        this.type = type;
+    }
+
+    // IndividualStorageHandler implementation
+    @Override
+    public void save(T data, String filePath) throws IOException {
+         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(filePath), data);
+    }
+
+    @Override
+    public T load(String filePath) throws IOException {
+         ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(filePath), type);
+    }
+
+    // ListStorageHandler implementation
+    @Override
+    public void saveList(List<T> dataList, String filePath) throws IOException {
+         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(filePath), dataList);
+    }
+
+   @Override
+    public List<T> loadList(String filePath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(filePath), objectMapper.getTypeFactory().constructCollectionType(List.class, type));
+
+        //return objectMapper.readValue(new File(filePath), new TypeReference<List<T>>() {});
+}
 }
--- SocialMediaApp/src/main/java/com/programming2/socialmediaapp/UserAccountManagement/UserRole.java
+++ SocialMediaApp/src/main/java/com/programming2/socialmediaapp/UserAccountManagement/UserRole.java
@@ -4,10 +4,50 @@
  */
 package com.programming2.socialmediaapp.UserAccountManagement;
 
-/**
- *
- * @author abdah
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRole {
+
+    private List<UserInfo> userInfoList;
+
     
+    public UserRole() {
+        this.userInfoList = new ArrayList<>();
+    }
+
+   
+    public void addUser(UserInfo user) {
+        if (user != null) {
+            userInfoList.add(user);
+            System.out.println("User added: " + user.getUserID());
+        } else {
+            System.out.println("Cannot add a null user.");
+        }
+    }
+
+    
+    public void saveList(ListStorageHandler<UserInfo> storageHandler, String filePath) {
+        try {
+            storageHandler.saveList(userInfoList, filePath);
+            System.out.println("User list saved to: " + filePath);
+        } catch (Exception e) {
+            System.err.println("Failed to save user list: " + e.getMessage());
+        }
+    }
+
+    
+    public void loadList(ListStorageHandler<UserInfo> storageHandler, String filePath) {
+        try {
+            userInfoList = storageHandler.loadList(filePath);
+            System.out.println("User list loaded from: " + filePath);
+        } catch (Exception e) {
+            System.err.println("Failed to load user list: " + e.getMessage());
+        }
+    }
+
+    
+    public List<UserInfo> getUserInfoList() {
+        return userInfoList;
+    }
 }
