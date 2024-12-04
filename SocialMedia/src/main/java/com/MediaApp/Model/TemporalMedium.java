69881d7e4488f137625eccaecaf6ccdc3b74b71b
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-12-04 20:02:10 +0200

    Delete Social Media Folder and validate class

--- SocialMedia/src/main/java/com/MediaApp/Model/TemporalMedium.java
+++
@@ -1,8 +0,0 @@
-package com.MediaApp.Model;
-
-import java.time.Instant;
-
-public interface TemporalMedium {
-    Instant getExpirationTime();
-    void setExpirationTime(Instant expirationTime);
-}
