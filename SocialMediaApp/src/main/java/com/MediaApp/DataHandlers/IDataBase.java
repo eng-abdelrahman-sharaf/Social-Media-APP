a032a1f1fcf525a74a45a05b020f753e820735b6
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-12-05 16:08:38 +0200

    Add getData method

--- SocialMediaApp/src/main/java/com/MediaApp/DataHandlers/IDataBase.java
+++ SocialMediaApp/src/main/java/com/MediaApp/DataHandlers/IDataBase.java
@@ -16,11 +16,14 @@ public interface IDataBase {
     // to apply singleton (only one instance of the class)
     public IDataBase getInstance(ListStorageHandler<IDataObject> handler);
     
-    
     // Read action [read object from the database]
     // Return the object if found, otherwise return null
     // The returned object should be a **clone** of the object in the database
     public IDataObject readObject(String ID);
+
+    // Read all objects from the database
+    // Return an array of **cloned** objects [deep cloning]
+    public IDataObject[] getData();
 
     // Create action [add new object to the database]
     // reload should be called inside this method (before adding the object)
@@ -35,8 +38,9 @@ public interface IDataBase {
     // updating an object in the database
     // reload should be called inside this method (before updating the object)
     // save should be called inside this method (after updating the object)
-    public void update(String ID , IDataObject object);
+    public void update(String ID, IDataObject object);
 
+    
     // Reload the database to get the latest data
     public void reload();
     
