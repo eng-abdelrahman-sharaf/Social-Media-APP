40777932ba31f1917e5d0d73acd99a71be9bd54d
Mohammed Hatem <mr.mohammedhatem@gmail.com>
2024-12-09 01:23:39 +0200

    final

--- SocialMediaApp/src/main/java/com/MediaApp/LoginPage/test.java
+++ SocialMediaApp/src/main/java/com/MediaApp/LoginPage/test.java
@@ -1,43 +1,29 @@
-package com.MediaApp.NewsFeed;
+package com.MediaApp.LoginPage;
 
-import com.MediaApp.ContentManagement.Content;
-import com.MediaApp.ContentManagement.IContent;
-import com.MediaApp.ContentManagement.Post;
-import com.MediaApp.Main;
-import com.MediaApp.UserAccountManagement.UserInfo;
-import com.MediaApp.UserAccountManagement.UserRoleDataBase;
-import com.gui.content_mangement_components.StageGetter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
-public class test1 extends Application {
+public class test extends Application {
 
     @Override
-
     public void start(Stage stage) {
         try {
+            int x = 5;
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/LoginPage/LoginPage.fxml"));
+            System.out.println(loader);
             Scene scene = new Scene(loader.load());
-
-
-
-            MainController controller = loader.getController();;
-            controller.start();
             stage.setScene(scene);
             stage.show();
+            stage.setResizable(false);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
+        System.out.println("asdf");
         launch(args);
     }
 }
--- SocialMediaApp/src/main/java/com/MediaApp/RequestsPage/RequestsPageController.java
+++ SocialMediaApp/src/main/java/com/MediaApp/RequestsPage/RequestsPageController.java
@@ -1,4 +1,58 @@
 package com.MediaApp.RequestsPage;
 
+import com.MediaApp.SuggestedUsers.UserNodeController;
+import com.MediaApp.UserAccountManagement.UserInfo;
+import com.MediaApp.UserAccountManagement.UserRoleDataBase;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class RequestsPageController {
+    @FXML
+    private VBox RequestsPane;
+
+    @FXML
+    public void initialize() {
+    }
+
+    public void setRequests(UserInfo User) {
+        UserNodeController userNodeController = new UserNodeController();
+        UserRoleDataBase userDB = UserRoleDataBase.getInstance(null);
+        List<UserInfo> users = new ArrayList<>();
+        System.out.println(User.getFriendsREquest());
+
+        for (String id : User.getFriendsREquest()) {
+            users.add((UserInfo) userDB.readObject(id));
+            System.out.println("akjbdlas");
+        }
+
+        for (UserInfo user : users) {
+            Button userNodeButton = (Button) userNodeController.createUserNode(user);
+            userNodeButton.setOnAction(event -> {
+
+                     List<String> ls =   User.getFriendsIDs();
+                     ls.add(user.getUserID());
+                     User.setFriendsIDs(ls);
+                    List<String> ls2 =   User.getFriendsREquest();
+                     ls2.remove(user.getUserID());
+                     User.setFriendsREquest(ls2);
+                     userNodeButton.setVisible(false);
+                RequestsPane.getChildren().remove(userNodeButton);
+
+            });
+
+            ImageView icon = new ImageView(new Image(Objects.requireNonNull(getClass().getResource(user.getProfilePhotoPath())).toExternalForm()));
+            icon.setFitHeight(20);
+            icon.setFitWidth(20);
+            userNodeButton.setGraphic(icon);
+            RequestsPane.getChildren().add(userNodeButton);
+            System.out.println("akjbdlas");
+        }
+    }
 }
