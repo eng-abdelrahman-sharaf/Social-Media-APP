626af591c6a2706897fe5db5fc52ae6d2518fd4f
Anwar <abd.ahm.anwar@gmail.com>
2024-12-12 17:59:17 +0200

    Add note

--- SocialMediaApp/src/main/java/com/notifcation/NotificationServiceImpl.java
+++ SocialMediaApp/src/main/java/com/notifcation/NotificationServiceImpl.java
@@ -4,10 +4,25 @@
  */
 package com.notifcation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author abdah
  */
-public class NotificationServiceImpl {
-    
+public class NotificationServiceImpl implements INotificationService {
+    private List<INotification> notifications = new ArrayList<>();
+
+    @Override
+    public void sendNotification(INotification notification) {
+        notifications.add(notification);
+    }
+
+    public List<INotification> getNotifications(String userId) {
+        return notifications.stream()
+            .filter(notification -> notification.getUserId().equals(userId) && !notification.isRead())
+            .collect(Collectors.toList());
+    }
 }
