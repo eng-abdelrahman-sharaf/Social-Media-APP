36a5fc6fe889cab1357cfd89a8affd75e59f3d91
Anwar <abd.ahm.anwar@gmail.com>
2024-12-13 04:54:55 +0200

    add notes to INotificationService

--- SocialMediaApp/src/main/java/com/notifcation/INotificationService.java
+++ SocialMediaApp/src/main/java/com/notifcation/INotificationService.java
@@ -5,10 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- *
- * @author abdah
- */
+// NotificationService Interface 
+// This interface defines the methods for handling notifications, 
+// ensuring that any class implementing it will have methods to send and fetch notifications.
 public interface INotificationService {
     void sendNotification(INotification notification);
     public List<INotification> getNotifications(String userId);
